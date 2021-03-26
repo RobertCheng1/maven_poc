3211a4b207125913073c7b5a96db342bcf2a4c07
unknown <chengpengxing@360buyAD.local>
2021-03-26 10:32:05 +0800

    Add big comments

--- src/main/java/com/company/mywebsite/HelloServlet.java
+++ src/main/java/com/company/mywebsite/HelloServlet.java
@@ -35,9 +35,13 @@ public class HelloServlet extends HttpServlet {
      * @throws ServletException
      * @throws IOException
      *
+     * 注意到这个pom.xml与前面我们讲到的普通Java程序有个区别，打包类型不是jar，而是war，表示Java Web Application Archive
+     * 我们应该如何运行这个war文件？
+     * 普通的Java程序是通过启动JVM，然后执行main()方法开始运行。但是Web应用程序有所不同，我们无法直接运行war文件，
+     * 必须先启动Web服务器，再由Web服务器加载我们编写的HelloServlet，这样就可以让HelloServlet处理浏览器发送的请求。
      *
      * 浏览器里请求： http://localhost:8080/mavenpoc/
-     * 细心的童鞋可能会问，为啥路径是/hello/而不是/？
+     * 细心的童鞋可能会问，为啥路径是/mavenpoc/ 而不是/？
      * 因为一个Web服务器允许同时运行多个Web App，而我们的Web App叫 mavenpoc (在 pom.xml 中指定的 finalName 为 mavenpoc)
      * 因此，第一级目录/mavenpoc 表示Web App的名字， 后面的 / 才是我们在HelloServlet中映射的路径。
      */
